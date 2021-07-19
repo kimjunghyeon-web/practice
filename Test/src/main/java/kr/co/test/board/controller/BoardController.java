3ebd00700496cc5c047ece2ebd418d5f4b2e9307
ccmedia <ccmedia@DESKTOP-6JPLDKM>
2021-07-19 17:32:44 +0900

    autowired 주입 안됨

--- Test/src/main/java/kr/co/test/board/controller/BoardController.java
+++ Test/src/main/java/kr/co/test/board/controller/BoardController.java
@@ -1,13 +1,27 @@
 package kr.co.test.board.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.co.test.board.service.BoardService;
+import kr.co.test.board.vo.BoardVO;
+
 @Controller
 public class BoardController {
+	@Autowired
+	private BoardService boardService;
+
 	@RequestMapping(value = "/list.do", method = { RequestMethod.GET })
-	public String list() {
+	public String list(Model model) {
+		List<BoardVO> list = new ArrayList<BoardVO>();
+		list = boardService.getBoardList();
+		model.addAttribute("list", list);
 		return "board/list.page";
 	}
 }
--- Test/src/main/webapp/WEB-INF/jsp/main/home.jsp
+++ Test/src/main/webapp/WEB-INF/jsp/main/home.jsp
@@ -2,16 +2,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ page trimDirectiveWhitespaces="true" %>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Insert title here</title>
-<meta charset="UTF-8" />
-</head>
-<body>
 <h1>메인페이지</h1>
 <div><a href="list.do">게시판</a></div>
 <div><a href="login.do">로그인</a></div>
 <div><a href="list.do">회원가입</a></div>
 </body>
-</html>
