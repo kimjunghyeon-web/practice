5b8cea0ab30ec3648b2ef45be80772aebfc03722
ccmedia <ccmedia@DESKTOP-6JPLDKM>
2021-07-20 16:36:25 +0900

    설정 잡기 게시판 및 상세화면 구현

--- Test/src/main/java/kr/co/test/board/controller/BoardController.java
+++ Test/src/main/java/kr/co/test/board/controller/BoardController.java
@@ -1,7 +1,10 @@
 package kr.co.test.board.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,5 +26,15 @@ public class BoardController {
 		list = boardService.getBoardList();
 		model.addAttribute("list", list);
 		return "board/list.page";
+	}
+
+	@RequestMapping(value = "/list_view.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public String boardView(HttpServletRequest req, Model model) {
+		HashMap<String, Object> param = new HashMap<>();
+		int bno = Integer.parseInt(req.getParameter("bno"));
+		param.put("bno", bno);
+		BoardVO view = boardService.getBoardView(param);
+		model.addAttribute("view", view);
+		return "board/list_view.page";
 	}
 }
